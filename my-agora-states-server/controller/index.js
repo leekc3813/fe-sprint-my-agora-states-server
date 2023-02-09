d917ebcfb2e0615bf29ae21ae77a72d20dc394ff
leekc3813 <93689662+leekc3813@users.noreply.github.com>
2023-02-09 09:53:30 +0900

    0209

---
+++ my-agora-states-server/controller/index.js
@@ -0,0 +1,25 @@
+const { agoraStatesDiscussions } = require("../repository/discussions");
+const discussionsData = agoraStatesDiscussions;
+
+const discussionsController = {
+  findAll: (req, res) => {
+    // TODO: 모든 discussions 목록을 응답합니다.
+    return res.json(discussionsData)
+  },
+
+  findById: (req, res) => {
+    // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
+    const {id} = req.params;
+    const data = discussionsData.find(discussion => discussion.id === Number(id));
+    if(data) {
+      return res.status(200).json(data)
+    }else {
+      return res.status(404).send('Not found');
+    }
+  }
+
+};
+
+module.exports = {
+  discussionsController,
+};
